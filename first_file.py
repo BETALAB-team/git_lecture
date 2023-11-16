28adaa7e76f10111415833c1c2557e1650cf5fa6
Mohamad H. Khajedehi <khajedehi.hasan@gmail.com>
2023-11-16 17:21:25 +0100

    mohamad modification

--- first_file.py
+++ first_file.py
@@ -11,20 +11,9 @@ data = pd.read_excel("data.xlsx", index_col = 0)
 print(data)
 
 print("Something new in the main branch")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("I uploaded everything on github.")
-print("New branch on github")
-=======
-print("I uploaded everything on github")
-print("Hi, It's Mohamad")
->>>>>>> e067367b26b894b887e36b6cc975f6d56b80d11f
-=======
 
 print("I uploaded everything on github.")
 print("New branch on github")
 
-"Enrico Prataviera: This is my change"
+print("Hi its Mohamad")
 
-##"It's Mohamad"
->>>>>>> 3a640b0d192a27d7ff69b2847e3002bfe698ff68
